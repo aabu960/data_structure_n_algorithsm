665ceb5ccb78050a418ab40ad4338f0116695c5c
aabu960 <aabu960@yahoo.com>
2025-03-17 19:03:19 +0000

    Solving Factorial Algorithsm Recursion implementation

--- Recursion_in_Algorithms.js
+++ Recursion_in_Algorithms.js
@@ -7,5 +7,14 @@ console.log(n);
 countdown(n-1);
 
 }
-
+// Factorial
 countdown(10);
+
+function factorial(n) {
+    if(n === 0) return 1; //Base Case
+
+return n * factorial(n-1); // Recussive Case
+
+}
+
+console.log(factorial(5));
