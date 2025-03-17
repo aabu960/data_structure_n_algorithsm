331fa34a74b7cdf63dc0bcbe3e3d4ebd44a9c009
aabu960 <aabu960@yahoo.com>
2025-03-17 19:11:35 +0000

    Solving Fabonacci Algorithsm Recursion implementation

--- Recursion_in_Algorithms.js
+++ Recursion_in_Algorithms.js
@@ -18,3 +18,17 @@ return n * factorial(n-1); // Recussive Case
 }
 
 console.log(factorial(5));
+
+
+// Factorial Using Iteration
+
+function factorialIteration(n) {
+    let result = 1;
+    for(let i = 2; i <=n; i++){
+        result *= i;
+
+    }
+    return result;
+
+}
+console.log(factorialIteration(10));
