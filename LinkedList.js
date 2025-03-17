961b15010ae062820927a9343a229ef7bfa4317d
aabu960 <aabu960@yahoo.com>
2025-03-17 17:38:49 +0000

    added linked list

---
+++ LinkedList.js
@@ -0,0 +1,21 @@
+/*  
+
+2️⃣ Types of Linked Lists
+There are three main types of linked lists:
+
+Singly Linked List – Each node points to the next node only.
+Doubly Linked List – Each node points to both next and previous nodes.
+Circular Linked List – The last node connects back to the first node, forming a loop.
+*/
+
+//Implementation
+
+class Node {
+    constructor(value) {
+      this.value = value;  // Data stored in node
+      this.next = null;  // Pointer to next node
+    }
+  
+
+
+}
