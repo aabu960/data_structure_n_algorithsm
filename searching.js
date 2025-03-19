dd800e397f08b37d1e61992563e2b3d79a29c80f
aabu960 <aabu960@yahoo.com>
2025-03-19 21:25:23 +0000

    added serching

---
+++ searching.js
@@ -0,0 +1,10 @@
+// SEARCHING
+// Binary Search 
+
+function binarySearch (arr, target) {
+    for(let i = 0; i<=Array.length;i++) {
+        if (arr ===target) return i; //fond
+
+    }
+    return -1; // not found
+}
