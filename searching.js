e25af79071e1977e9418a65428446dd353bab6d3
aabu960 <aabu960@yahoo.com>
2025-03-19 21:32:15 +0000

    added serching

--- searching.js
+++ searching.js
@@ -1,6 +1,6 @@
 // SEARCHING
 // Binary Search 
-
+//Linear 
 function binarySearch (arr, target) {
     for(let i = 0; i<=Array.length;i++) {
         if (arr ===target) return i; //fond
@@ -8,3 +8,26 @@ function binarySearch (arr, target) {
     }
     return -1; // not found
 }
+
+console.log(binarySearch([10,52,0,30,10]));
+
+// Binary search
+
+function binarySearches(arr, target) {
+    left= 0;
+    right= arr.length -1;
+
+    while (left <= right ) {
+        let mid = Math.floor((left + right) / 2);
+
+
+if(arr[mid]=== target) return mid //found
+else if (arr[mid <target] ) left = mid +1;
+else right = mid -1;
+
+
+    } 
+    return -1
+
+}
+
