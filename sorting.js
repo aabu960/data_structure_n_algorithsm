04013024ebd8b2bb34ab5db1367184427555bd75
aabu960 <aabu960@yahoo.com>
2025-03-19 19:50:25 +0000

    added sorted code bubb

--- sorting.js
+++ sorting.js
@@ -39,4 +39,4 @@ function bubbleSort(arr) {
     return ary;
   } 
 
-  console.log(bubbledsort([5, 9, 8, 4, 2])); // [2, 3, 4, 5, 8]
+  console.log(bubbledsort([5, 9, 8, 4, 3])); // [2, 3, 4, 5, 8]
