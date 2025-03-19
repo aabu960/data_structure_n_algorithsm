5d14e52af769e37cb648884bac2093451a170c4a
aabu960 <aabu960@yahoo.com>
2025-03-19 22:32:39 +0000

    aded two num solution

--- coding_Problem_solved/two_num.js
+++ coding_Problem_solved/two_num.js
@@ -1,15 +1,10 @@
 function twoSum(nums, target) {
-    let map = new Map(); // Stores {number: index}
-    
     for (let i = 0; i < nums.length; i++) {
-        let complement = target - nums[i]; // What number we need to find
-        
-        if (map.has(complement)) {
-            return [map.get(complement), i];
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
         }
-        
-        map.set(nums[i], i); // Store current number with index
     }
-    
     return [];
 }
